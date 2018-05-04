7274ddebcce93309e16e955757fae27c99f70211
beemyfriend <plzbeemyfriend@gmail.com>
2018-05-04 00:25:26 -0400

    2013, 2014 doesn't have toc, will need to do a lot of owrk

--- 205KG.R
+++ 205KG.R
@@ -127,3 +127,116 @@ new_nodes <- V(g2016)[!name %in% names(V(g2017))] %>%
   as_data
 new_edges <- E(g2016)
 g2017 + new_nodes
+
+setwd('traversing_graphs_in_R/')
+library(igraph)
+library(tidyverse)
+g2017 <- read_graph('data/g2017.graphml', 'graphml')
+g2016 <- read_graph('data/g2016.graphml', 'graphml')
+g2015 <- read_graph('data/g2015.graphml', 'graphml')
+
+node_list <- ls() %>%
+  .[str_detect(., 'g20\\d\\d')] %>%
+  lapply(function(x){
+    get(x, envir = .GlobalEnv) %>%
+      igraph::as_data_frame('vertices')
+  }) %>%
+  bind_rows() %>%
+  select(-id) %>%
+  distinct()
+
+edge_list <- ls() %>%
+  .[str_detect(., 'g20\\d\\d')] %>%
+  lapply(function(x){
+    get(x, envir = .GlobalEnv) %>%
+      igraph::as_data_frame('edges')
+  }) %>%
+  bind_rows() %>%
+  distinct()
+
+g <- graph_from_data_frame(edge_list, T, node_list)
+
+degree(g) %>%
+  .[names(.) %in% V(g)[nodeType == 'AUTHOR']$name] %>% 
+  sort(T) %>%
+  .[. > 3]
+
+text2013 <- pdftools::pdf_text('useR! Abstracts/useR2013.pdf')
+toc2013 <- pdftools::pdf_toc('useR! Abstracts/useR2013.pdf')
+
+abstracts2013 <- toc2013$children %>%
+  lapply(function(x){
+    title1 <- x$title
+  
+      x$children %>%
+      lapply(function(y){
+        title2 <- y$title
+
+        y$children %>%
+          lapply(function(z){
+            tibble(
+              title3 = z$title
+            )
+          }) %>%
+          bind_rows() %>%
+          mutate(title2 = title2)
+      }) %>%
+      bind_rows %>%
+      mutate(title1 = title1)
+  }) %>%
+  bind_rows() %>%
+  mutate(page = 6 + 1:nrow(.),
+         page = ifelse(page >= 137, page + 1, page)) %>%
+  rbind(
+    tibble(
+      title3 = "Asymmetric Volatility Transmission in Airline Related Companies in Stock Markets",
+      title2 = 'Poster',
+      title1 = "Regular Posters",
+      page = 137
+    ) 
+  ) %>%
+  mutate(
+    title2 = ifelse(page >= 137, 'Poster', title2),
+    abstractBody = text2013[page] %>%
+      str_replace('^[^\\n]*\\n', ''),
+    authors = map2(title3, abstractBody, function(x,y){
+      final_word <- x %>%
+        str_replace_all('[:punct:]', ' ') %>%
+        str_trim() %>%
+        str_split(' ') %>%
+        unlist() %>%
+        .[length(.)]
+      
+      temp <- y %>%
+        str_split('\\n') %>%
+        unlist() 
+      
+      (1:length(temp))[str_detect(temp, str_c(final_word, '.*$'))][1] %>%
+        (function(x){
+          if(!is.na(x)){
+            r = ((x+1):(x+4))
+            temp[r] %>% 
+              str_trim() %>% 
+              .[!str_detect(., '^\\?')] %>%
+              {
+                if(str_detect(.[1], '^\\d|^Authors|')){
+                  .[2:length(.)]
+                } else {
+                  .
+                }
+              } %>%
+              (function(y){
+                r = (1:length(y))[str_detect(y, regex('^\\d|^keywords|^department|^contact|^school^dept\\.', ignore_case = T))][1] - 1 
+                if(is.na(r)){
+                  y
+                } else {
+                  y[1:r]
+                } 
+              })
+          } else {
+            NA
+          }
+     })
+    }))
+
+#138, 137, 151, 154, 129, 124, 116, 107, 97, 80, 73, , 70, 60, 24
